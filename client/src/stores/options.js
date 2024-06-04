07407ce3316d5b9cb44f0586d7878db6aea3408d
innTall <avg999.trade@gmail.com>
2024-06-04 08:43:18 +0200

    build: create grid options modal

---
+++ client/src/stores/options.js
@@ -0,0 +1,90 @@
+import { defineStore } from "pinia";
+import { ref, onMounted } from "vue";
+export const useOptionsStore = defineStore(
+  "options",
+  () => {
+    const options = ref({});
+    const minFirstOrder = ref();
+    const gridOrders = ref();
+    const firstBuyLevel = ref();
+    const orderRiseCoef = ref();
+    const buyLevelsCoef = ref();
+    const priceRound = ref();
+    const amountRound = ref();
+
+    async function getOptions() {
+      let uri = "http://localhost:3000/options";
+      try {
+        const response = await fetch(uri);
+        const data = await response.json();
+        options.value = data;
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    async function postOptions() {
+      const uri = "http://localhost:3000/options/add";
+      const Options = {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          //options: options.value,
+          minFirstOrder: minFirstOrder.value,
+          gridOrders: gridOrders.value,
+          firstBuyLevel: firstBuyLevel.value,
+          orderRiseCoef: orderRiseCoef.value,
+          buyLevelsCoef: buyLevelsCoef.value,
+          priceRound: priceRound.value,
+          amountRound: amountRound.value,
+        }),
+      };
+      try {
+        const response = await fetch(uri, Options);
+        const data = await response.json();
+        minFirstOrder.value = data.minFirstOrder;
+        gridOrders.value = data.gridOrders;
+        firstBuyLevel.value = data.firstBuyLevel;
+        orderRiseCoef.value = data.orderRiseCoef;
+        buyLevelsCoef.value = data.buyLevelsCoef;
+        priceRound.value = data.priceRound;
+        amountRound.value = data.amountRound;
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    async function updateOptions() {
+      let uri = "http://localhost:3000/api/update";
+      try {
+        const response = await fetch(uri);
+        const data = await response.json();
+        minFirstOrder.value = data.minFirstOrder;
+        gridOrders.value = data.gridOrders;
+        firstBuyLevel.value = data.firstBuyLevel;
+        orderRiseCoef.value = data.orderRiseCoef;
+        buyLevelsCoef.value = data.buyLevelsCoef;
+        priceRound.value = data.priceRound;
+        amountRound.value = data.amountRound;
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    return {
+      options,
+      minFirstOrder,
+      gridOrders,
+      firstBuyLevel,
+      orderRiseCoef,
+      buyLevelsCoef,
+      priceRound,
+      amountRound,
+      postOptions,
+      updateOptions,
+    };
+  },
+  {
+    persist: true,
+  }
+);
